c109d699b80c6ef5402b279c6a848fd845992e2e
alexsnow <alexsandr.styazhkov@gmail.com>
2025-12-30 14:21:57 +0500

    modified:   src/consts.ts 	modified:   src/pages/blog/index.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SnowSolutions';
+export const SITE_DESCRIPTION = 'SnowSolutions сайт посвящённый системеному анализу с примерами кода и объяснением как это работает ';
