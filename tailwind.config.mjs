f6f78d2ff561e1d40b14f249a3d0d50151722d3f
alexsnow <alexsandr.styazhkov@gmail.com>
2026-01-26 20:14:08 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';
+// tailwind.config.mjs
+import typography from '@tailwindcss/typography'
 
+/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		typography,
-	],
-};
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}', // все файлы проекта
+  ],
+  theme: {
+    extend: {}, // сюда можно добавлять кастомные стили
+  },
+  plugins: [
+    typography, // подключаем плагин типографики
+  ],
+}
