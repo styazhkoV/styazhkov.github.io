03c78e1e163d6c0b0ea1e04d6d1c880180d11674
alexsnow <alexsandr.styazhkov@gmail.com>
2026-01-27 00:31:31 +0500

    modified:   astro.config.mjs 	modified:   src/layouts/BaseLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+// import tailwind from '@astrojs/tailwind';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://styazhkov.github.io',
-  integrations: [tailwind()],
+  integrations: [],
 
   // Если ты тестируешь локально, trailingSlash может влиять на 404
   trailingSlash: 'ignore',
